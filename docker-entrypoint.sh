28ca470d822c569e77dd49dee91d25235cb6b338
Bence <145575284+Garga180@users.noreply.github.com>
2024-11-18 23:58:04 +0100

    .env update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,8 +2,10 @@
 
 ./wait-for-it.sh mysql:3306 --timeout=30 --strict -- echo "MySQL is up!"
 
+compose install
 php artisan migrate --force
 php artisan db:seed --force
 php artisan storage:link
+php artisan key:generate
 
 exec apache2-foreground
